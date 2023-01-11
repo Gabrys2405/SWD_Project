ffe403a0ecccf14f75b190f85a9d5cae9f4d7fe6
Piotr <psikorski@student.agh.edu.pl>
2023-01-11 16:28:13 +0100

    Poprawki redakcyjne

--- src/hotel_loader.py
+++ src/hotel_loader.py
@@ -1,6 +1,9 @@
+# Autor: Piotr Sikorski
+
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional, Iterable
+from typing import TYPE_CHECKING
 if TYPE_CHECKING:
+    from typing import Optional, Iterable
     import numpy as np
 
 import pandas as pd
