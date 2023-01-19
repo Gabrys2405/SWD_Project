c002ec2c88fcf7bb1b885ad81df0b880e5b8f6a5
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-01-19 15:00:56 +0100

    ranking_rsm

--- app/system_wyboru_hoteli/funkcje_rankingowe/rsm.py
+++ app/system_wyboru_hoteli/funkcje_rankingowe/rsm.py
@@ -2,6 +2,20 @@ import numpy as np
 import pandas as pd
 
 
+def square_search(pu, pA0, pA1):
+    if pA0.shape == pA1.shape:
+        for i in range(pA0.shape[0]):
+            if pA0[i] > pA1[i]:
+                if pA1[i] > pu[i] or pu[i] > pA0[i]:
+                    return False, 0
+            elif pA0[i] < pA1[i]:
+                if pA0[i] > pu[i] or pu[i] > pA1[i]:
+                    return False, 0
+            else:
+                return False, 0
+
+        return True, np.prod(np.abs(pA0 - pA1))  # multiply
+
 def ranking_rsm(
         kryteria_hoteli: pd.DataFrame,
         punkty_docelowe: np.ndarray,
@@ -24,11 +38,48 @@ def ranking_rsm(
         DataFrame zawierający jedną kolumnę z wartościami rankingowymi, indeksowanymi
         jak w kryteria_hoteli
     """
+    wartosci_funkcji_kryterialnej = np.zeros((kryteria_hoteli.shape[0],))
 
-    # TODO
-    # Podpowiedzi:
-    #  - kryteria_hoteli.values jest macierzą NumPy, zmiany w niej wpływają na DataFrame
-    #  - kryteria_hoteli.drop(indeks) usuwa wiersz
-    #  - kryteria_hoteli.index[i] zwraca indeks do użycia w metodzie drop()
-    # Istotne jest, aby zachować pierwotne indeksowanie z kryteria_hoteli
-    raise NotImplementedError()
+    for iu in range(kryteria_hoteli.shape[0]):
+        u = kryteria_hoteli[iu, :]  # punkt u - do określenia wartości
+
+        suma_pol = 0  # suma pól prostokątów
+        pary_wektorow_prostokatow = []  # wektory tworzące potrzebne prostokąty
+        pola_prostokatow = []  # pola prostokątów powyżej
+
+        # Szukaj par wierzchołków tworzących prostokąty zawierające punkt u
+        for iA0 in range(punkty_docelowe.shape[0]):
+            vA0 = punkty_docelowe[iA0, :]
+            for iA1 in range(punkty_status_quo.shape[0]):
+                vA1 = punkty_status_quo[iA1, :]
+                w_prostokacie, pole = square_search(u, vA0, vA1)
+                if w_prostokacie:
+                    suma_pol += pole
+                    pary_wektorow_prostokatow.append((vA0, vA1))
+                    pola_prostokatow.append(pole)
+
+        # Zliczenie wag prostokątów
+        wagi: np.ndarray = np.array(pola_prostokatow) / suma_pol
+
+        # Zliczenie odległości punktów prostokątów
+        odleglosci: List[float] = []
+        for vA0, vA1 in pary_wektorow_prostokatow:
+            dA0 = np.sqrt(np.sum((vA0 - u) ** 2))
+            dA1 = np.sqrt(np.sum((vA1 - u) ** 2))
+            odleglosci.append(dA0 / (dA0 + dA1))
+
+        # Złożenie do wartości funkcji kryterialnej
+        wartosci_funkcji_kryterialnej[iu] = np.sum(wagi * np.array(odleglosci))
+
+    #ranking
+    pozycja = np.array(range(wartosci_funkcji_kryterialnej.shape[0])) + 1
+    ranking = np.array([pozycja, wartosci_funkcji_kryterialnej]).T
+    ranking = ranking[ranking[:, 1].argsort()[::-1]]
+    ranking = ranking[:,:1]
+    ranking = pd.DataFrame(ranking)
+
+    return ranking
+
+
+
+
